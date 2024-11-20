734c2dc6494c9f438d5e9160640ab751cd4496b8
Gopi88-Aut <gngopi88@gmail.com>
2024-11-20 17:31:49 +0530

    commit

---
+++ Nov'24/Ex_17112024_Literals_Variables/Lab017_Literals_Task1.py
@@ -0,0 +1,11 @@
+a= int(input("Enter 1st number"))
+b=int(input("enter 2nd number"))
+c=int(input ("enter 3rd number"))
+sum = a+b
+sub = a+b-c
+mul = a*b*c
+div = a/b+c
+print ("sum of a&b is", sum)
+print ("sub of a&b is", sub)
+print ("mul of a&b is", mul)
+print ("div of a&b is", div)
